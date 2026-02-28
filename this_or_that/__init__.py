d2aba833ca4d760d46abfc720a118bcb449158b7
P Carmel Marie F. Zagre <108446176+appgpk@users.noreply.github.com>
2026-02-27 22:35:59 -0800

    Update __init__.py

--- this_or_that/__init__.py
+++ this_or_that/__init__.py
@@ -3,21 +3,28 @@ from otree.api import *
 c = cu
 
 doc = ''
+
 class C(BaseConstants):
     # built-in constants
     NAME_IN_URL = 'this_or_that'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 5
+
+
 class Subsession(BaseSubsession):
     pass
+    
 class Group(BaseGroup):
     pass
+    
 class Player(BasePlayer):
     choice = models.IntegerField()
 def vars_for_template(player: Player):
     return dict(
             image_path='shoes/shoe{}.jpg'.format(player.round_number)
         )
+    
 class MakeChoice(Page):
     form_model = 'player'
+    
 page_sequence = [MakeChoice]
