e9479783df199a8a0b9c48acf1dfeb6811d85863
P Carmel Marie F. Zagre <108446176+appgpk@users.noreply.github.com>
2026-02-27 22:47:45 -0800

    Update __init__.py

--- this_or_that/__init__.py
+++ this_or_that/__init__.py
@@ -1,31 +1,30 @@
-
 from otree.api import *
-c = cu
 
 doc = ''
 
 class C(BaseConstants):
-    # built-in constants
     NAME_IN_URL = 'this_or_that'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 5
 
-
 class Subsession(BaseSubsession):
     pass
-    
+
 class Group(BaseGroup):
     pass
-    
+
 class Player(BasePlayer):
-    choice = models.IntegerField()
+    choice = models.IntegerField(
+        choices=[[1, 'A'], [2, 'B']],
+        widget=widgets.RadioSelect
+    )
 
 class MakeChoice(Page):
     form_model = 'player'
-    staticmethod
-     def vars_for_template(player: Player):
-        image_path='_/static/shoes/shoe{}.jpg'.format(player.round_number)
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        image_path = 'shoes/shoe{}.jpg'.format(player.round_number)
         return dict(image_path=image_path)
 
-   
 page_sequence = [MakeChoice]
