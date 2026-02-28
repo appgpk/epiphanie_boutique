9649d4d4dfdd29bb6cb259c6d2749ca29b102391
P Carmel Marie F. Zagre <108446176+appgpk@users.noreply.github.com>
2026-02-27 23:28:46 -0800

    Update __init__.py

--- demographic_survey/__init__.py
+++ demographic_survey/__init__.py
@@ -14,32 +14,65 @@ class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
-    age = models.IntegerField(label="Quel est votre âge ?")
+    age = models.IntegerField(
+        label="Quel est votre âge ?",
+        min=10, max=99,
+        blank=True
+    )
+
     gender = models.StringField(
         label="Quel est votre genre ?",
-        choices=["Femme", "Homme", "Autre"]
+        choices=["Femme", "Homme", "Autre", "Je préfère ne pas répondre"],
+        blank=True
     )
+
     city = models.StringField(
-        label="Dans quelle ville habitez-vous ?"
+        label="Dans quelle ville habitez-vous ?",
+        blank=True
     )
 
     shoe_size = models.IntegerField(
-        label="Quelle est votre pointure ?"
+        label="Quelle est votre pointure ?",
+        choices=[str(x) for x in range(35, 46)] + ["Je ne sais pas / Je préfère ne pas répondre"],
+        blank=True
     )
 
-    buy_shoes = models.StringField(
+    budget = models.StringField(
+        label="Quel budget mettez-vous en général pour une paire de chaussures ?",
+        choices=[
+            "Moins de 5 000 FCFA",
+            "5 000 – 10 000 FCFA",
+            "10 000 – 15 000 FCFA",
+            "15 000 – 25 000 FCFA",
+            "Plus de 25 000 FCFA",
+            "Je préfère ne pas répondre",
+        ],
+        blank=True
+    )
+
+    frequency = models.StringField(
         label="À quelle fréquence achetez-vous des chaussures ?",
         choices=[
             "1 fois par an",
             "2–3 fois par an",
-            "plus de 3 fois par an"
-        ]
+            "1 fois par mois",
+            "Plus d’1 fois par mois",
+            "Je préfère ne pas répondre",
+        ],
+        blank=True
     )
 
 class Introduction(Page):
     @staticmethod
     def is_displayed(player: Player):
         return player.round_number == 1
+        
+class Demographics(Page):
+    form_model = 'player'
+    form_fields = ['age', 'gender', 'city', 'shoe_size', 'budget', 'frequency']
 
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1
 
-page_sequence = [Introduction]
+page_sequence = [Introduction, Demographics]
