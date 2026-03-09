f5bf7f8cc42937845d3f99c078db1fd3a9fb51d9
P Carmel Marie F. Zagre <108446176+appgpk@users.noreply.github.com>
2026-03-08 22:40:18 -0700

    Update __init__.py

--- demographic_survey/__init__.py
+++ demographic_survey/__init__.py
@@ -137,13 +137,15 @@ class Ranking(Page):
     def before_next_page(player, timeout_happened):
 
         if player.rank1 == 1:
-            player.participant.treatment = 1
+            player.treatment = 1
 
         elif player.rank2 == 1:
-            player.participant.treatment = 2
+            player.treatment = 2
 
         elif player.rank3 == 1:
-            player.participant.treatment = 3
+            player.treatment = 3
+         
+        player.participant.vars['treatment'] = player.treatment
 
 
 
