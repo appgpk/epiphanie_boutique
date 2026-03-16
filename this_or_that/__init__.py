28190ada354d820579dc3008b1a6390d75f5b488
P Carmel Marie F. Zagre <108446176+appgpk@users.noreply.github.com>
2026-03-15 22:33:32 -0700

    Update __init__.py

--- this_or_that/__init__.py
+++ this_or_that/__init__.py
@@ -241,12 +241,24 @@ class C(BaseConstants):
         'slingback/medium/very-high/stiletto/category-2/shoes07.png'],
 }
 
+PAIRS_TREATMENT_1 = [
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (2, 3),
+    (2, 4),
+    (2, 5),
+]
+
 
 class Subsession(BaseSubsession):
     pass
 
+
 class Group(BaseGroup):
     pass
+
+
     
 class Player(BasePlayer):
     treatment = models.IntegerField(initial=1)
